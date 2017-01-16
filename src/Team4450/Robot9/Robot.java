0231c2fa9dc1add91dcc463d677cafe181389015
Richard Corn <crossle86@users.noreply.github.com>
2017-01-15 22:36:07 -0800

    Integrate Grip Pipeline class into vision tracking and updated camera feed.

--- src/Team4450/Robot9/Robot.java
+++ src/Team4450/Robot9/Robot.java
@@ -31,7 +31,7 @@ import com.ctre.CANTalon.*;
 
 public class Robot extends SampleRobot 
 {
-  static final String  	PROGRAM_NAME = "RAC9-01.13.17-03";
+  static final String  	PROGRAM_NAME = "RAC9-01.14.17-01";
 
   // Motor CAN ID/PWM port assignments (1=left-front, 2=left-rear, 3=right-front, 4=right-rear)
   CANTalon				LFCanTalon, LRCanTalon, RFCanTalon, RRCanTalon, LSlaveCanTalon, RSlaveCanTalon;
